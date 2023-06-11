865d234304174d5d6befc4f4306e50fd8b301667
Emin <emin.isaev97@gmail.com>
2023-06-11 07:14:28 +0700

    some more fixes to deployment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
